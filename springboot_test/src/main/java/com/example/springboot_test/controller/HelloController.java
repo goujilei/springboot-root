65fe6584791b7611fca5edff3e31d62aab0ac590
git <920242033@qq.com>
2025-09-23 23:09:06 +0800

    jenkins测试

--- springboot_test/src/main/java/com/example/springboot_test/controller/HelloController.java
+++ springboot_test/src/main/java/com/example/springboot_test/controller/HelloController.java
@@ -37,7 +37,7 @@ public class HelloController {
             return "error";
         }
 
-        return "success";
+        return "success1111";
 
 /*        if ("0".equals(flag)){
             throw new RuntimeException("模拟异常");
