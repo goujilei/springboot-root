d2829bbebdd8b50d079c1a87c75fae1e20839d99
git <920242033@qq.com>
2026-02-01 22:03:22 +0800

    Revert "git回退测试3"
    This reverts commit 56eff4c29f97dc0c53884e34539849538411b3c8.

--- springboot_test/src/main/java/com/example/springboot_test/cron/CronTask001.java
+++ springboot_test/src/main/java/com/example/springboot_test/cron/CronTask001.java
@@ -20,13 +20,12 @@ public class CronTask001 {
 
     // 每5秒执行一次
     //@Scheduled(fixedRate = 5000)
-    @Scheduled(cron = "*/5  * * * * ?")
+    //@Scheduled(cron = "*/5  * * * * ?")
     public void runEveryFiveSeconds() {
         log.info("==========开始执行生成生成批量资金文件任务==========");
 
         LocalDateTime now = LocalDateTime.now();
         log.info("任务执行时间：{}", now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
-        runAtOneAM5();
         log.info("==========执行生成生成批量资金文件任务成功==========");
     }
 
@@ -42,17 +41,5 @@ public class CronTask001 {
 
     public void runAtOneAM2() {
         System.out.println("凌晨1点执行任务");
-    }
-
-    public void runAtOneAM3() {
-        System.out.println("凌晨3点执行任务");
-    }
-
-    public void runAtOneAM4() {
-        System.out.println("凌晨4点执行任务");
-    }
-
-    public void runAtOneAM5() {
-        System.out.println("凌晨5点执行任务");
     }
 }
